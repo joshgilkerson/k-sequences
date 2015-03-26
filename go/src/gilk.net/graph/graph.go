09357db033d00eb4032c34aec4404b1039e93495
Josh Gilkerson <josh@gilk.net>
2015-03-25 23:11:57 -0700

    Add error handling.

--- go/src/gilk.net/graph/graph.go
+++ go/src/gilk.net/graph/graph.go
@@ -1,51 +1,64 @@
 package graph
 
+import "errors"
+import "fmt"
+
 type Status int
 type Weight int
 type Node int
 type NodePair [2]Node
 
 type WeightedUndirectedGraph struct {
-	Rows [][]Weight
+  Rows [][]Weight
 }
 
 func NewWeightedUndirectedGraph(nodeCount int) *WeightedUndirectedGraph {
-	rows := make([][]Weight, 0, nodeCount)
-	for i := 0; i < nodeCount; i++ {
-		rows = append(rows, make([]Weight, i+1))
-		// 0 -> Linearized[0:1]
-		// 1 -> Linearized[1:3]
-		// 2 -> Linearized[3:6]
-	}
-	return &WeightedUndirectedGraph{rows}
+  rows := make([][]Weight, 0, nodeCount)
+  for i := 0; i < nodeCount; i++ {
+    rows = append(rows, make([]Weight, i+1))
+    // 0 -> Linearized[0:1]
+    // 1 -> Linearized[1:3]
+    // 2 -> Linearized[3:6]
+  }
+  return &WeightedUndirectedGraph{rows}
 }
 
-func sortedNodes(nodes NodePair) (Node, Node) {
-	if nodes[0] < nodes[1] {
-		return nodes[0], nodes[1]
-	} else {
-		return nodes[1], nodes[0]
-	}
+func (g *WeightedUndirectedGraph) getEdge(nodes NodePair) (w *Weight, e error) {
+  w = nil
+  e = nil
+  for _, node := range nodes {
+    if int(node) > len(g.Rows) {
+      e = errors.New(fmt.Sprintf(
+        "Node out of bounds: %d",
+        node))
+      return
+    }
+  }
+  if nodes[0] == nodes[1] {
+    e = errors.New("Self-edges are not supported.")
+    return
+  }
+  if nodes[0] < nodes[1] {
+    w = &(g.Rows[nodes[1]][nodes[0]])
+  } else {
+    w = &(g.Rows[nodes[0]][nodes[0]])
+  }
+  return
 }
 
-func (g *WeightedUndirectedGraph) getEdge(nodes NodePair) *Weight {
-	if int(nodes[0]) > len(g.Rows) {
-		return nil
-	} else {
-		n1, n2 := sortedNodes(nodes)
-		return &(g.Rows[n2][n1])
-	}
+func (g *WeightedUndirectedGraph) Set(nodes NodePair, weight Weight) error {
+  if edge, error := g.getEdge(nodes); error == nil {
+    *edge = weight
+    return nil
+  } else {
+    return error
+  }
 }
 
-func (g *WeightedUndirectedGraph) Set(nodes NodePair, weight Weight) {
-	(*g.getEdge(nodes)) = weight
-}
-
-func (g *WeightedUndirectedGraph) Get(nodes NodePair) Weight {
-	edge := g.getEdge(nodes)
-	if edge == nil {
-		return 0
-	} else {
-		return *edge
-	}
+func (g *WeightedUndirectedGraph) Get(nodes NodePair) (Weight, error) {
+  if edge, error := g.getEdge(nodes); error == nil {
+    return *edge, nil
+  } else {
+    return 0, error
+  }
 }
--- go/src/gilk.net/graph/graph_test.go
+++ go/src/gilk.net/graph/graph_test.go
@@ -3,29 +3,32 @@ package graph
 import "testing"
 
 func TestNew(t *testing.T) {
-	g := NewWeightedUndirectedGraph(2)
-	if len(g.Rows) != 2 {
-		t.Errorf("Rows should have length 2, has %d",
-			len(g.Rows))
-		t.Fail()
-	}
-	if len(g.Rows[len(g.Rows)-1]) != 2 {
-		t.Errorf("Rows last element  should have length 2, has %d",
-			len(g.Rows))
-		t.Fail()
-	}
+  g := NewWeightedUndirectedGraph(2)
+  if len(g.Rows) != 2 {
+    t.Errorf("Rows should have length 2, has %d",
+      len(g.Rows))
+  }
+  if len(g.Rows[len(g.Rows)-1]) != 2 {
+    t.Errorf("Rows last element  should have length 2, has %d",
+      len(g.Rows))
+  }
 }
 
 func TestSimple(t *testing.T) {
-	g := NewWeightedUndirectedGraph(10)
-	for i := 0; i < 10; i++ {
-		for j := 0; j < 10; j++ {
-			w := int(g.Get(NodePair{Node(i), Node(j)}))
-			if w != 0 {
-				t.Errorf("Wrong weight at (%d, %d). Expected 0, Found %d.",
-					i, j, w)
-				t.Fail()
-			}
-		}
-	}
+  g := NewWeightedUndirectedGraph(10)
+  for i := 0; i < 10; i++ {
+    for j := 0; j < 10; j++ {
+      if j == i {
+        continue
+      }
+      if w, e := g.Get(NodePair{Node(i), Node(j)}); e == nil {
+        if int(w) != 0 {
+          t.Errorf("Wrong weight at (%d, %d). Expected 0, Found %d.",
+            i, j, w)
+        }
+      } else {
+        t.Errorf("Get(%d, %d) failed. %s", i, j, e.Error())
+      }
+    }
+  }
 }
